432f21b62f8bd5e40cf557bbedc9897cfba61826
dp.sokolov <dp.sokolov@jet.su>
2019-11-06 20:14:01 +0300

    add main, change host

--- convertor_init.py
+++ convertor_init.py
@@ -1,8 +1,8 @@
+import json
+from collections import namedtuple
 import asyncio
 import socket
-import json
 import logging
-from collections import namedtuple
 
 from api import routes
 
@@ -42,48 +42,49 @@ def parse_request(request: bytes) -> tuple:
     return method, url, params
 
 
-host = 'localhost'
-port = 8888
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.setblocking(False)
-server_socket.bind((host, port))
-server_socket.listen(10)
+if __name__ == '__main__':
+    host = '0.0.0.0'
+    port = 8888
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.setblocking(False)
+    server_socket.bind((host, port))
+    server_socket.listen(10)
 
-main_loop = asyncio.get_event_loop()
-main_loop.set_debug(enabled=True)
+    main_loop = asyncio.get_event_loop()
+    main_loop.set_debug(enabled=True)
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger("asyncio")
-
-
-async def request_handler(conn):
-    req_bytes = await main_loop.sock_recv(conn, 1024)
-    if req_bytes:
-        request = Request(*parse_request(req_bytes))
-        logger.debug(f'request: {request.method} {request.url}  {request.params}')
-        if request.url in routes:
-            body, status = await routes[request.url](request.method, request.params)
-            logger.debug(f'response: {body} {status}')
-        else:
-            status = 404
-            body = ''
-        response = make_response(body, status)
-        await main_loop.sock_sendall(conn, response)
-    conn.close()
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("asyncio")
 
 
-async def http_server(sock, loop):
-    logger.debug('converter server started.....')
-    while True:
-        conn, addr = await loop.sock_accept(sock)
-        loop.create_task(request_handler(conn))
+    async def request_handler(conn):
+        req_bytes = await main_loop.sock_recv(conn, 1024)
+        if req_bytes:
+            request = Request(*parse_request(req_bytes))
+            logger.debug(f'request: {request.method} {request.url}  {request.params}')
+            if request.url in routes:
+                body, status = await routes[request.url](request.method, request.params)
+                logger.debug(f'response: {body} {status}')
+            else:
+                status = 404
+                body = ''
+            response = make_response(body, status)
+            await main_loop.sock_sendall(conn, response)
+        conn.close()
 
 
-try:
-    main_loop.run_until_complete(http_server(server_socket, main_loop))
-except KeyboardInterrupt:
-    pass
-finally:
-    main_loop.close()
-    server_socket.close()
+    async def http_server(sock, loop):
+        logger.debug('converter server started.....')
+        while True:
+            conn, addr = await loop.sock_accept(sock)
+            loop.create_task(request_handler(conn))
+
+
+    try:
+        main_loop.run_until_complete(http_server(server_socket, main_loop))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        main_loop.close()
+        server_socket.close()
