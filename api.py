d636b48fdbccde76d352fee3daecf762351fdaa1
dp.sokolov <dp.sokolov@jet.su>
2019-11-07 19:13:59 +0300

    refactor api

--- api.py
+++ api.py
@@ -31,66 +31,64 @@ async def disconnect_db():
 
 
 async def database_post(method, params):
-    if method == 'POST':
-        try:
-            merge = bool(params['merge'])
-            rates = params['rates']
-        except (KeyError, ValueError):
-            result = error_result('missing the query param: merge, or it has invalid value, allowed (0,1)')
-            status = 400
-        else:
-            transaction = await database.transaction()
-            try:
-                if not merge:  # invalidate all rates in the table
-                    await database.execute(query=INVALIDATE_ALL_RATES)
-                await database.execute_many(query=UPSERT_RATE, values=rates)
-            except (ArgumentError, KeyError):
-                await transaction.rollback()
-                result = error_result('invalid rates data')
-                status = 400
-            except Exception as e:
-                await transaction.rollback()
-                result = error_result(f'database error: {repr(e)}')
-                status = 500
-            else:
-                await transaction.commit()
-                result = {'result': "rates updated"}
-                status = 200
-    else:
+    if method not in ('POST', 'PUT', 'PATCH'):
         result = error_result(f'method {method} not allowed')
-        status = 405
+        return result, 405
+
+    try:
+        merge = bool(params['merge'])
+        rates = params['rates']
+    except (KeyError, ValueError):
+        result = error_result('missing the query param: merge, or it has invalid value, allowed (0,1)')
+        return result, 400
+
+    transaction = await database.transaction()
+    try:
+        if not merge:  # invalidate all rates in the table
+            await database.execute(query=INVALIDATE_ALL_RATES)
+        await database.execute_many(query=UPSERT_RATE, values=rates)
+    except (ArgumentError, KeyError):
+        await transaction.rollback()
+        result = error_result('invalid rates data')
+        status = 400
+    except Exception as e:
+        await transaction.rollback()
+        result = error_result(f'database error: {repr(e)}')
+        status = 500
+    else:
+        await transaction.commit()
+        result = {'result': "rates updated"}
+        status = 200
 
     return result, status
 
 
 async def convert_get(method, params):
-    if method == 'GET':
-        try:
-            from_curr = params['from'].upper()
-            to_curr = params['to'].upper()
-            from_amount = float(params['amount'])
-        except (KeyError, ValueError):
-            result = error_result('missing mandatory param(s) or invalid amount value')
-            status = 400
-        else:
-            xrate = await database.fetch_one(
-                query=GET_RATE,
-                values={'from_curr': from_curr, 'to_curr': to_curr}
-            )
-            if not xrate:
-                result = error_result(f'unknown currency pair')
-                status = 400
-            elif not xrate['valid']:
-                result = error_result(f'no valid rate for this currency pair')
-                status = 400
-            else:
-                rate = xrate['rate']
-                to_amount = from_amount * rate
-                result = {'to_amount': f'{to_amount:.2f}'}
-                status = 200
-    else:
+    if method != 'GET':
         result = error_result(f'method {method} not allowed')
-        status = 405
+        return result, 405
+
+    try:
+        from_curr = params['from'].upper()
+        to_curr = params['to'].upper()
+        from_amount = float(params['amount'])
+    except (KeyError, ValueError):
+        result = error_result('missing mandatory param(s) or invalid amount value')
+        return result, 400
+
+    xrate = await database.fetch_one(query=GET_RATE, values={'from_curr': from_curr, 'to_curr': to_curr})
+    if not xrate:
+        result = error_result(f'unknown currency pair')
+        status = 400
+    elif not xrate['valid']:
+        result = error_result(f'no valid rate for this currency pair')
+        status = 400
+    else:
+        rate = xrate['rate']
+        to_amount = from_amount * rate
+        result = {'to_amount': f'{to_amount:.2f}'}
+        status = 200
+
     return result, status
 
 
