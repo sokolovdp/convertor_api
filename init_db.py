a123a2252c2855c76c8469568c417c346f862fe8
dp.sokolov <dp.sokolov@jet.su>
2019-11-07 12:43:11 +0300

    create unique multicolumn index

--- init_db.py
+++ init_db.py
@@ -2,10 +2,37 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.exc import DBAPIError
-from psycopg2.errorcodes import DUPLICATE_TABLE
 
 import tables
 
+initial_xrates = [
+    {
+        'from_currency': 'USD',
+        'to_currency': 'RUB',
+        'rate': 63.70,
+        'valid': True
+    },
+    {
+        'from_currency': 'RUB',
+        'to_currency': 'USD',
+        'rate': 0.016,
+        'valid': True
+
+    },
+    {
+        'from_currency': 'EUR',
+        'to_currency': 'RUB',
+        'rate': 70.54,
+        'valid': True
+    },
+    {
+        'from_currency': 'RUB',
+        'to_currency': 'EUR',
+        'rate': 0.014,
+        'valid': True
+
+    }
+]
 
 if __name__ == '__main__':
     database_url = tables.DATABASE_URL
@@ -16,17 +43,12 @@ if __name__ == '__main__':
     db_connection = db_engine.connect()
     try:
         tables.xrates.create(db_connection)
+        # create unique multicolumn index
+        db_connection.execute('CREATE UNIQUE INDEX from_to_idx ON xrates (from_currency, to_currency)')
     except DBAPIError as db_error:
-        if db_error.orig.pgcode == DUPLICATE_TABLE:
-            print('\nxrate table already exists!\n')
-            pass
-        else:
-            raise db_error
+        print(f'\nDB error code: {db_error.orig.pgcode}\n')
 
-    # create multicolumn index
-    db_connection.execute('CREATE INDEX from_to_idx ON xrates (from_currency, to_currency)')
-
-    # query = tables.xrates.insert()
-    # db_connection.execute(query, )
+    query = tables.xrates.insert()
+    db_connection.execute(query, initial_xrates)
 
     print('\nxrate table created and populated with initial data\n')
