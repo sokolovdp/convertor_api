ee713da75cadb08d189e16ece662757228a42a7c
dp.sokolov <dp.sokolov@jet.su>
2019-11-07 17:07:31 +0300

    proper shutdown

--- convertor_server.py
+++ convertor_server.py
@@ -2,6 +2,7 @@ import json
 from collections import namedtuple
 import asyncio
 import socket
+import signal
 import logging
 
 import api
@@ -69,12 +70,11 @@ async def request_handler(main_loop, conn):
     conn.close()
 
 
-async def http_server(sock, loop):
-    await api.connect_db()
-    logger.debug('converter server started.....')
+async def http_server(sock, main_loop):
+    logger.info('converter server started.....')
     while True:
-        conn, addr = await loop.sock_accept(sock)
-        loop.create_task(request_handler(loop, conn))
+        conn, addr = await main_loop.sock_accept(sock)
+        main_loop.create_task(request_handler(main_loop, conn))
 
 
 def main():
@@ -88,10 +88,12 @@ def main():
     main_loop.set_debug(enabled=convertor_config.DEBUG_MODE)
 
     try:
+        main_loop.run_until_complete(api.connect_db())
         main_loop.run_until_complete(http_server(server_socket, main_loop))
     except KeyboardInterrupt:
         pass
     finally:
+        main_loop.run_until_complete(api.disconnect_db())
         main_loop.close()
         server_socket.close()
 
