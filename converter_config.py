29e8fd1cc4c793260dff5fa63752d668e77ad76d
dp.sokolov <dp.sokolov@jet.su>
2019-11-10 18:45:57 +0300

    minor fix

--- converter_config.py
+++ converter_config.py
@@ -8,7 +8,7 @@ LOGGING_FORMAT = "%(asctime)s,%(msecs)d %(levelname)s: %(message)s"
 LOGGING_DATE_FORMAT = "%H:%M:%S"
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8888
-MAX_REQUEST_LENGTH = 4096  # no bytes to get from a socket at once
+MAX_REQUEST_LENGTH = 2048  # no bytes to get from a socket at once
 LISTEN_BACKLOG = 10  # maximum number of queued connections
 
 # database settings:
